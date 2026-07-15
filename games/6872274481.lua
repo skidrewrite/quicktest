3ae3d362408d97ff2baf3f6d6f4abd1e2a812e34
R12sa <144062428+R12sa@users.noreply.github.com>
2026-07-14 17:47:19 -0700

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -10114,28 +10114,30 @@ run(function()
                         local localPosition = entitylib.character.RootPart.Position
                         for _, v in items do
                             if tick() - (v:GetAttribute('ClientDropTime') or 0) < 2 then continue end
-                            if isnetworkowner(v) and Network.Enabled and entitylib.character.Humanoid.Health > 0 then
-                                v.CFrame = CFrame.new(localPosition - Vector3.new(0, 3, 0))
-                            end
-
+                            
+                            -- Check if item is in range
                             if (localPosition - v.Position).Magnitude <= Range.Value then
                                 if Lower.Enabled and (localPosition.Y - v.Position.Y) < (entitylib.character.HipHeight - 1) then continue end
-                                task.spawn(function()
-                                    bedwars.Client:Get(remotes.PickupItem):CallServerAsync({
-                                        itemDrop = v
-                                    }):andThen(function(suc)
-                                        if suc and bedwars.SoundList then
-                                            bedwars.SoundManager:playSound(bedwars.SoundList.PICKUP_ITEM_DROP)
-                                            local sound = bedwars.ItemMeta[v.Name].pickUpOverlaySound
-                                            if sound then
-                                                bedwars.SoundManager:playSound(sound, {
-                                                    position = v.Position,
-                                                    volumeMultiplier = 0.9
-                                                })
+                                
+                                -- Only pickup if network owner
+                                if isnetworkowner(v) and Network.Enabled and entitylib.character.Humanoid.Health > 0 then
+                                    task.spawn(function()
+                                        bedwars.Client:Get(remotes.PickupItem):CallServerAsync({
+                                            itemDrop = v
+                                        }):andThen(function(suc)
+                                            if suc and bedwars.SoundList then
+                                                bedwars.SoundManager:playSound(bedwars.SoundList.PICKUP_ITEM_DROP)
+                                                local sound = bedwars.ItemMeta[v.Name].pickUpOverlaySound
+                                                if sound then
+                                                    bedwars.SoundManager:playSound(sound, {
+                                                        position = v.Position,
+                                                        volumeMultiplier = 0.9
+                                                    })
+                                                end
                                             end
-                                        end
+                                        end)
                                     end)
-                                end)
+                                end
                             end
                         end
                     end
@@ -10145,22 +10147,24 @@ run(function()
         end,
         Tooltip = 'Picks up items from a farther distance'
     })
+    
     Range = PickupRange:CreateSlider({
         Name = 'Range',
         Min = 1,
-        Max = 10,
+        Max = 50,
         Default = 10,
         Suffix = function(val)
             return val == 1 and 'stud' or 'studs'
         end
     })
+    
     Network = PickupRange:CreateToggle({
         Name = 'Network TP',
         Default = true
     })
+    
     Lower = PickupRange:CreateToggle({Name = 'Feet Check'})
 end)
-
 	
 run(function()
 	local Scaffold
